fae3acd9b28b7ee4882804d43d1b448637399346
Piotr Malecki <ideals-carders.0k@icloud.com>
2025-08-22 15:02:50 +0100

    test: add unit tests

---
+++ apps/web/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
